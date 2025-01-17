c6409c94770adb9d96d37460e6bb7ce227a9b32a
Nicolas Gihoul <ngihoul@hotmail.com>
2025-01-17 11:07:51 +0100

    Hiding second navbar when not authenticated

--- src/app/_components/Layout/NavBar/NavBar.tsx
+++ src/app/_components/Layout/NavBar/NavBar.tsx
@@ -1,15 +1,24 @@
+'use client';
+
+import { useAuth } from '@/lib/contexts/authContext';
 import './NavBar.css';
 
 export default function NavBar() {
+    const { userId } = useAuth();
     return (
-        <nav className="nav-container">
-            <div className="nav-wrapper wrapper">
-                <ul>
-                    <li><a href="#">Roster</a></li>
-                    <li><a href="#">League</a></li>
-                    <li><a href="#">Stats</a></li>
-                </ul>
-            </div>
-        </nav>
+        // TODO : create navbar when not authenticated
+        <>
+            {userId && (
+                <nav className="nav-container">
+                <div className="nav-wrapper wrapper">
+                    <ul>
+                        <li><a href="#">Roster</a></li>
+                        <li><a href="#">League</a></li>
+                        <li><a href="#">Stats</a></li>
+                    </ul>
+                </div>
+            </nav>
+            )}
+        </>
     );
 }
