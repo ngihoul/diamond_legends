c284be296c37bdd5be1485281974c4d3e3eaa868
Nicolas Gihoul <ngihoul@hotmail.com>
2025-01-20 14:05:54 +0100

    Using Country instead of Nationality

--- src/lib/services/countries.tsx
+++ src/lib/services/countries.tsx
@@ -1,8 +1,8 @@
-import { Nationality } from "@/lib/models/nationality.model";
+
 import { API_URL } from "../../../config";
+import { Country } from "../models/country.model";
 
-
-export default async function getCountries(): Promise<Nationality[]> {
+export default async function getCountries(): Promise<Country[]> {
     try {
         const response = await fetch(`${API_URL}/country`);
         const data = await response.json();
