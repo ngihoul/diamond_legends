d7e6122865f86ccd4d0419e8e8db857b4f376825
Nicolas Gihoul <ngihoul@hotmail.com>
2025-01-21 11:39:18 +0100

    Adding container

--- src/app/game/dashboard/page.tsx
+++ src/app/game/dashboard/page.tsx
@@ -5,7 +5,7 @@ import { useGame } from "@/lib/contexts/gameContext"
 export default function Dashboard() {
     const { teamSelected } = useGame();
     return (
-        <div>
+        <div className="dashboard-container">
             <div className="heading-left">
                 <h2>Dashboard</h2>
                 <p className="subtitle">Un résumé de ton equipe</p>
