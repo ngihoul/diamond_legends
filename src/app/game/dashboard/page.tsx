3a99099303e1bb57c514436dfe425b7b5d65f22e
Nicolas Gihoul <ngihoul@hotmail.com>
2025-01-21 11:37:22 +0100

    Add titles

--- src/app/game/dashboard/page.tsx
+++ src/app/game/dashboard/page.tsx
@@ -6,6 +6,10 @@ export default function Dashboard() {
     const { teamSelected } = useGame();
     return (
         <div>
+            <div className="heading-left">
+                <h2>Dashboard</h2>
+                <p className="subtitle">Un résumé de ton equipe</p>
+            </div>
             <p>Team selectionnée : { teamSelected }</p>
         </div>
     );
