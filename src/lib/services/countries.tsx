74faaf605cd631a2f82ebbffbf0da2e882b3748a
Nicolas Gihoul <ngihoul@hotmail.com>
2025-01-21 11:21:57 +0100

    Using axios instead of fetch

--- src/lib/services/countries.tsx
+++ src/lib/services/countries.tsx
@@ -1,12 +1,11 @@
-
 import { API_URL } from "../../../config";
 import { Country } from "../models/country.model";
+import apiClient from "./api";
 
 export default async function getCountries(): Promise<Country[]> {
     try {
-        const response = await fetch(`${API_URL}/country`);
-        const data = await response.json();
-        return data;
+        const response = await apiClient.get(`${API_URL}/country`);
+        return response.data;
     } catch (error) {
         console.error('Error fetching nationalities:', error);
         return [];
