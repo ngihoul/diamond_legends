e1cce5ad4fa8e3c31509fb34db98f92e3eae8fdc
Nicolas Gihoul <ngihoul@hotmail.com>
2025-01-21 09:53:09 +0100

    Optimizing NavBar

--- src/app/_components/Layout/NavBar/NavBar.tsx
+++ src/app/_components/Layout/NavBar/NavBar.tsx
@@ -2,23 +2,40 @@
 
 import { useAuth } from '@/lib/contexts/authContext';
 import './NavBar.css';
+import Link from 'next/link';
+import { useGame } from '@/lib/contexts/gameContext';
+import Image from 'next/image';
+
+import ball from '@/public/img/ball_without_bg.png';
 
 export default function NavBar() {
     const { userId } = useAuth();
+    const { teamSelected } = useGame();
+
     return (
         // TODO : create navbar when not authenticated
         <>
-            {userId && (
-                <nav className="nav-container">
+            <nav className="nav-container">
                 <div className="nav-wrapper wrapper">
-                    <ul>
-                        <li><a href="#">Roster</a></li>
-                        <li><a href="#">League</a></li>
-                        <li><a href="#">Stats</a></li>
-                    </ul>
+                    <Image src={ball} alt="ball" width={31} height={40} />
+                    {userId && (
+                        <Link href={'/game/load'}>Sélectionne ton équipe !</Link>
+                    )}
+
+                    {!userId && (
+                        <Link className="nav-text" href={'/auth/signup'}>Rejoins le Hall of Fame, inscris-toi !</Link>
+                    )}
+
+                    {teamSelected && (
+                        <ul>
+                            <li><a href="#">Dashboard</a></li>
+                            <li><a href="#">Roster</a></li>
+                            <li><a href="#">League</a></li>
+                            <li><a href="#">Stats</a></li>
+                        </ul>
+                    )}
                 </div>
             </nav>
-            )}
         </>
     );
 }
