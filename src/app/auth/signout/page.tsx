f5cfa72a340ba1c458d573a8868263a4f2115916
Nicolas Gihoul <ngihoul@hotmail.com>
2025-01-21 10:48:58 +0100

    Solving issue when signout

--- src/app/auth/signout/page.tsx
+++ src/app/auth/signout/page.tsx
@@ -2,13 +2,17 @@
 
 import { useAuth } from "@/lib/contexts/authContext";
 import { useToaster } from "@/lib/contexts/toasterContext";
-import { redirect } from "next/navigation";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function SignOut() {
     const { logout } = useAuth();
     const { showToast } = useToaster();
-
-    logout();
-    showToast("Deconnexion réussie", 'success');
-    redirect('/');
+    const router = useRouter();
+    
+    useEffect(() => {
+        logout();
+        showToast("Deconnexion réussie", 'success');
+        router.push('/');
+    }, []);
 }
